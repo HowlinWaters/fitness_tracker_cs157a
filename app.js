a188d01fcfb3cef5f0cf9258055120d58573cda0
Karthik Renuprasad <karthikrenuprasad@gmail.com>
2024-05-02 16:42:07 -0700

    working till location

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
 import express from "express";
-import { getUsers, getUser, createUser } from "./database.js";
+import {
+  getUsers,
+  getUser,
+  getUserFromLogin,
+  createUser,
+  updateUser,
+  deleteUser,
+  getFitnessGoalUser,
+  createFitnessGoal,
+  updateFitnessGoal,
+  deleteFitnessGoal,
+  getFitnessGoal,
+  getLocation,
+  createLocation,
+  updateLocations,
+  deleteLocation,
+} from "./database.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
+
+//For Users table
 
 app.get("/users", async (req, res) => {
   const users = await getUsers();
@@ -16,23 +34,163 @@ app.get("/users/:id", async (req, res) => {
   res.send(user);
 });
 
+app.get("/users/:Username/:Password", async (req, res) => {
+  const Username = req.params.Username;
+  const Password = req.params.Password;
+  const user = await getUserFromLogin(Username, Password);
+  res.send(user);
+});
+
 app.post("/createusers", async (req, res) => {
-  const { Username, Password, FName, LName, DOB, Weight, Height, Email, Avatar } =
-    req.body
+  const { Username, Password, FName, LName, DOB, Weight, Height, Email } =
+    req.body;
   const user = await createUser(
     Username,
-    Password, 
+    Password,
+    FName,
+    LName,
+    DOB,
+    Weight,
+    Height,
+    Email
+  );
+  res.status(201).send(user);
+});
+
+app.put("/updateusers", async (req, res) => {
+  const {
+    Username,
+    Password,
     FName,
     LName,
     DOB,
     Weight,
     Height,
     Email,
-    Avatar
-  )
-  res.status(201).send(user)
+    UserID,
+  } = req.body;
+  const user = await updateUser(
+    Username,
+    Password,
+    FName,
+    LName,
+    DOB,
+    Weight,
+    Height,
+    Email,
+    UserID
+  );
+  res.status(201).send(user);
 });
 
+app.delete("/deleteusers/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await deleteUser(id);
+  res.send(user);
+});
+//end of user table
+
+//start of fitnessgoals table
+app.get("/fitnessgoalsuser/:id", async (req, res) => {
+  const id = req.params.id;
+  const fitnessgoals = await getFitnessGoalUser(id);
+  res.send(fitnessgoals);
+});
+
+app.get("/fitnessgoal/:id", async (req, res) => {
+  const id = req.params.id;
+  const fitnessgoals = await getFitnessGoal(id);
+  res.send(fitnessgoals);
+});
+
+app.post("/createfitnessgoals", async (req, res) => {
+  const { UserID, Descriptions, CaloriesBurnt, CaloriesConsumed } = req.body;
+  const fitnessgoals = await createFitnessGoal(
+    UserID,
+    Descriptions,
+    CaloriesBurnt,
+    CaloriesConsumed
+  );
+  res.status(201).send(fitnessgoals);
+});
+
+app.put("/updatefitnessgoals", async (req, res) => {
+  const {
+    UserID,
+    Descriptions,
+    CaloriesBurnt,
+    CaloriesConsumed,
+    FitnessGoalID,
+  } = req.body;
+  const fitnessgoals = await updateFitnessGoal(
+    UserID,
+    Descriptions,
+    CaloriesBurnt,
+    CaloriesConsumed,
+    FitnessGoalID
+  );
+  res.status(201).send(fitnessgoals);
+});
+
+app.delete("/deletefitnessgoals/:id", async (req, res) => {
+  const id = req.params.id;
+  const fitnessgoals = await deleteFitnessGoal(id);
+  res.send(fitnessgoals);
+});
+//end of fitnessgoals
+
+//start of location
+app.get("/location/:id", async (req, res) => {
+  const id = req.params.id;
+  const location = await getLocation(id);
+  res.send(location);
+});
+
+app.post("/createlocations", async (req, res) => {
+  const { Address, City, State, BuildingName } = req.body;
+  const location = await createLocation(Address, City, State, BuildingName);
+  res.status(201).send(location);
+});
+
+app.put("/updatelocations", async (req, res) => {
+  const { Address, City, State, BuildingName, LocationID } = req.body;
+  const location = await updateLocations(
+    Address,
+    City,
+    State,
+    BuildingName,
+    LocationID
+  );
+  res.status(201).send(location);
+});
+
+app.delete("/deletelocations/:id", async (req, res) => {
+  const id = req.params.id;
+  const location = await deleteLocation(id);
+  res.send(location);
+});
+//end of location
+
+//start of activities
+//end of activities
+
+//start of trackactivities
+//end of trackactivities
+
+//start of activitylocation
+//end of activitylocation
+
+//start of profile
+//end of profile
+
+//start of favlocationwall
+//end of favlocationwall
+
+//start of milestones
+//end fo milestones
+
+//start of comment
+//end of comment
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
--- database.js
+++ database.js
@@ -1,35 +1,221 @@
-import mysql from 'mysql2'
+import mysql from "mysql2";
 
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-const dbconnection = mysql.createPool({
-    host:process.env.HOST,
+const dbconnection = mysql
+  .createPool({
+    host: process.env.HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-}).promise()
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
 
-//gets all users
-export async function getUsers(){
-    const [result] = await dbconnection.query("SELECT * FROM User")
-    return result
+//start of users
+export async function getUsers() {
+  const [result] = await dbconnection.query("SELECT * FROM User");
+  return result;
 }
 
-export async function getUser(id){
-    const [result] = await dbconnection.query(`
+export async function getUser(id) {
+  const [result] = await dbconnection.query(
+    `
     SELECT *
     FROM User
-    WHERE UserID = ?`
-    , [id])
-    return result[0]
+    WHERE UserID = ?`,
+    [id]
+  );
+  return result[0];
 }
 
-export async function createUser(Username, Password, FName, LName, DOB, Weight, Height, Email, Avatar){
-    const [result] = await dbconnection.query(`
-    INSERT INTO User (Username, Password, FName, LName, DOB, Weight, Height, Email, Avatar)
-    VALUES(?,?,?,?,?,?,?,?,?)
-    `, [Username, Password, FName, LName, DOB, Weight, Height, Email, Avatar])
-    const id = result.insertId
-    return getUser(id)
+export async function getUserFromLogin(Username, Password) {
+  const [result] = await dbconnection.query(
+    `
+      SELECT UserID FROM User WHERE Username = ? AND Password = ?`,
+    [Username, Password]
+  );
+  return result[0];
 }
+
+export async function createUser(
+  Username,
+  Password,
+  FName,
+  LName,
+  DOB,
+  Weight,
+  Height,
+  Email
+) {
+  const [result] = await dbconnection.query(
+    `
+    INSERT INTO User (Username, Password, FName, LName, DOB, Weight, Height, Email)
+    VALUES(?,?,?,?,?,?,?,?)
+    `,
+    [Username, Password, FName, LName, DOB, Weight, Height, Email]
+  );
+  const id = result.insertId;
+  return getUser(id);
+}
+
+export async function updateUser(
+  Username,
+  Password,
+  FName,
+  LName,
+  DOB,
+  Weight,
+  Height,
+  Email,
+  UserID
+) {
+  const [result] = await dbconnection.query(
+    `
+        UPDATE User
+        SET Username = ?, Password = ?, FName = ?, LName = ?, DOB = ?, Weight = ?, Height = ?, Email = ?
+        WHERE UserID = ?
+    `,
+    [Username, Password, FName, LName, DOB, Weight, Height, Email, UserID]
+  );
+  return getUser(UserID);
+}
+
+export async function deleteUser(UserID) {
+  const [result] = await dbconnection.query(
+    `
+        DELETE FROM User WHERE UserID = ?
+    `,
+    [UserID]
+  );
+  return result.affectedRows;
+}
+//end of users
+
+//start of fitnessgoals
+export async function getFitnessGoalUser(id) {
+  const [result] = await dbconnection.query(
+    `SELECT * FROM FitnessGoal WHERE UserID = ?`,
+    [id]
+  );
+  return result[0];
+}
+
+export async function getFitnessGoal(id) {
+  const [result] = await dbconnection.query(
+    `SELECT * FROM FitnessGoal WHERE FitnessGoalID = ?`,
+    [id]
+  );
+  return result[0];
+}
+
+export async function createFitnessGoal(
+  UserID,
+  Descriptions,
+  CaloriesBurnt,
+  CaloriesConsumed
+) {
+  const [result] = await dbconnection.query(
+    `INSERT INTO FitnessGoal (UserID, Descriptions, CaloriesBurnt, CaloriesConsumed)
+    VALUES (?, ?, ?, ?)`,
+    [UserID, Descriptions, CaloriesBurnt, CaloriesConsumed]
+  );
+  const id = result.insertId;
+  return getFitnessGoal(id);
+}
+
+export async function updateFitnessGoal(
+  UserID,
+  Descriptions,
+  CaloriesBurnt,
+  CaloriesConsumed,
+  FitnessGoalID
+) {
+  const [result] = await dbconnection.query(
+    `
+    UPDATE FitnessGoal 
+    SET UserID = ?, Descriptions = ?, CaloriesBurnt = ?, CaloriesConsumed = ?
+    WHERE FitnessGoalID = ?`,
+    [UserID, Descriptions, CaloriesBurnt, CaloriesConsumed, FitnessGoalID]
+  );
+  return getFitnessGoal(FitnessGoalID);
+}
+
+export async function deleteFitnessGoal(FitnessGoalID) {
+  const [result] = await dbconnection.query(
+    `
+        DELETE FROM FitnessGoal WHERE FitnessGoalID = ?
+      `,
+    [FitnessGoalID]
+  );
+  return result.affectedRows;
+}
+//end of fitnessgoals
+
+//start of location
+export async function getLocation(id) {
+  const [result] = await dbconnection.query(
+    `SELECT * FROM Location WHERE LocationID = ?`,
+    [id]
+  );
+  return result[0];
+}
+
+export async function createLocation(Address, City, State, BuildingName) {
+  const [result] = await dbconnection.query(
+    `INSERT INTO Location (Address, City, State, BuildingName)
+    VALUES (?, ?, ?, ?)`,
+    [Address, City, State, BuildingName]
+  );
+  const id = result.insertId;
+  return getLocation(id);
+}
+
+export async function updateLocations(
+  Address,
+  City,
+  State,
+  BuildingName,
+  LocationID
+) {
+  const [result] = await dbconnection.query(
+    `
+    UPDATE Location 
+    SET Address = ?, City = ?, State = ?, BuildingName = ?
+    WHERE LocationID = ?`,
+    [Address, City, State, BuildingName, LocationID]
+  );
+  return getLocation(LocationID);
+}
+
+export async function deleteLocation(LocationID) {
+  const [result] = await dbconnection.query(
+    `
+    DELETE FROM Location WHERE LocationID = ?
+    `,
+    [LocationID]
+  );
+  return result.affectedRows;
+}
+//end of location
+
+//start of activities
+//end of activities
+
+//start of trackactivities
+//end of trackactivities
+
+//start of activitylocation
+//end of activitylocation
+
+//start of profile
+//end of profile
+
+//start of favlocationwall
+//end of favlocationwall
+
+//start of milestones
+//end fo milestones
+
+//start of comment
+//end of comment
