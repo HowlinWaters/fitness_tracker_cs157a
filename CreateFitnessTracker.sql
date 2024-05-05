081b9aceafe682f4c5a8deb2ad1f8829d8a0a216
HowlinWaters <116417667+HowlinWaters@users.noreply.github.com>
2024-05-04 23:44:38 -0700

    Added indexes for tables in database.

--- CreateFitnessTracker.sql
+++ CreateFitnessTracker.sql
@@ -89,11 +89,32 @@ CREATE TABLE Comment (
     FOREIGN KEY (ProfileID) REFERENCES Profile(ProfileID) ON DELETE CASCADE 
 );
 
+CREATE INDEX UserIndex USING BTREE ON User(UserID);
+CREATE INDEX FitnessGoalIndex USING BTREE ON FitnessGoal(FitnessGoalID);
+CREATE INDEX LocationIndex USING BTREE ON Location(LocationID);
+CREATE INDEX ActivitiesIndex USING BTREE ON Activities(ActivityID);
+CREATE INDEX ProfileIndex USING BTREE ON Profile(ProfileID);
+CREATE INDEX FavLocationIndex USING BTREE ON FavLocationWall(FavWallID);
+CREATE INDEX MilestonesIndex USING BTREE ON Milestones(MilestoneID);
+
 -- example
 INSERT INTO User (Username, Password, FName, LName, DOB, Weight, Height, Email) 
 VALUES ('bob', 'basd', 'dsf', 'dsf', '1985-05-15', 'dsf', 'dsf', 'dsf');
 
+INSERT INTO FitnessGoal (UserID, Descriptions, CaloriesBurnt, CaloriesConsumed)
+VALUES (1, "blob", 100,  100);
 
+INSERT INTO Location (Address, City, State, BuildingName)
+VALUES ("bobadb", "dsfadsf", "asda", "A");
+
+INSERT INTO Location (Address, City, State, BuildingName)
+VALUES ("bobadb", "dsfadsf", "asda", "A");
+
+INSERT INTO Activities (ActivityName, DurationTime, TotalCaloriesBurnt)
+VALUES ("asdf", 23, 32);
+
+INSERT INTO Profile (ProfileID, ProfileName)
+VALUES (1, "bibly")
 
 
 
