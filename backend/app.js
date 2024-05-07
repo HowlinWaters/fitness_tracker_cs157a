fda2f1941a7818902424dd6ad3d3d369aa8b2a87
Karthik Renuprasad <karthikrenuprasad@gmail.com>
2024-05-06 19:03:35 -0700

    working with backend folder

---
+++ backend/app.js
@@ -0,0 +1,347 @@
+import express from "express";
+import cors from "cors";
+const app = express();
+import {
+  getUsers,
+  getUser,
+  getUserFromLogin,
+  createUser,
+  updateUser,
+  deleteUser,
+  getFitnessGoalUser,
+  createFitnessGoal,
+  updateFitnessGoal,
+  deleteFitnessGoal,
+  getFitnessGoal,
+  getLocation,
+  createLocation,
+  updateLocations,
+  deleteLocation,
+  getActivities,
+  createActivities,
+  updateActivities,
+  deleteActivities,
+  getTracksActivities,
+  createTracksActivities,
+  getActivityLocation,
+  createActivityLocation,
+  getProfile,
+  createProfile,
+  updateProfile,
+  getMilestones,
+  createMilestone,
+  updateMilestone,
+  deleteMilestone,
+  getProfileMilestones,
+  createProfileMilestones,
+  getComment,
+  createComment,
+  updateComment
+} from "./database.js";
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }));
+
+//For Users table
+
+app.get("/users", async (req, res) => {
+  const users = await getUsers();
+  res.send(users);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await getUser(id);
+  res.send(user);
+});
+
+app.get("/users/:Username/:Password", async (req, res) => {
+  const Username = req.params.Username;
+  const Password = req.params.Password;
+  const user = await getUserFromLogin(Username, Password);
+  res.send(user);
+});
+
+app.post("/createusers", async (req, res) => {
+  const { Username, Password, FName, LName, DOB, Weight, Height, Email } =
+    req.body;
+  const user = await createUser(
+    Username,
+    Password,
+    FName,
+    LName,
+    DOB,
+    Weight,
+    Height,
+    Email
+  );
+  res.status(201).send(user);
+});
+
+app.put("/updateusers", async (req, res) => {
+  const {
+    Username,
+    Password,
+    FName,
+    LName,
+    DOB,
+    Weight,
+    Height,
+    Email,
+    UserID,
+  } = req.body;
+  const user = await updateUser(
+    Username,
+    Password,
+    FName,
+    LName,
+    DOB,
+    Weight,
+    Height,
+    Email,
+    UserID
+  );
+  res.status(201).send(user);
+});
+
+app.delete("/deleteusers/:id", async (req, res) => {
+  const id = req.params.id;
+  const user = await deleteUser(id);
+  res.send(user);
+});
+//end of user table
+
+//start of fitnessgoals table
+app.get("/fitnessgoalsuser/:id", async (req, res) => {
+  const id = req.params.id;
+  const fitnessgoals = await getFitnessGoalUser(id);
+  res.send(fitnessgoals);
+});
+
+app.get("/fitnessgoal/:id", async (req, res) => {
+  const id = req.params.id;
+  const fitnessgoals = await getFitnessGoal(id);
+  res.send(fitnessgoals);
+});
+
+app.post("/createfitnessgoals", async (req, res) => {
+  const { UserID, Descriptions, CaloriesBurnt, CaloriesConsumed } = req.body;
+  const fitnessgoals = await createFitnessGoal(
+    UserID,
+    Descriptions,
+    CaloriesBurnt,
+    CaloriesConsumed
+  );
+  res.status(201).send(fitnessgoals);
+});
+
+app.put("/updatefitnessgoals", async (req, res) => {
+  const {
+    UserID,
+    Descriptions,
+    CaloriesBurnt,
+    CaloriesConsumed,
+    FitnessGoalID,
+  } = req.body;
+  const fitnessgoals = await updateFitnessGoal(
+    UserID,
+    Descriptions,
+    CaloriesBurnt,
+    CaloriesConsumed,
+    FitnessGoalID
+  );
+  res.status(201).send(fitnessgoals);
+});
+
+app.delete("/deletefitnessgoals/:id", async (req, res) => {
+  const id = req.params.id;
+  const fitnessgoals = await deleteFitnessGoal(id);
+  res.send(fitnessgoals);
+});
+//end of fitnessgoals
+
+//start of location
+app.get("/location/:id", async (req, res) => {
+  const id = req.params.id;
+  const location = await getLocation(id);
+  res.send(location);
+});
+
+app.post("/createlocations", async (req, res) => {
+  const { Address, City, State, BuildingName } = req.body;
+  const location = await createLocation(Address, City, State, BuildingName);
+  res.status(201).send(location);
+});
+
+app.put("/updatelocations", async (req, res) => {
+  const { Address, City, State, BuildingName, LocationID } = req.body;
+  const location = await updateLocations(
+    Address,
+    City,
+    State,
+    BuildingName,
+    LocationID
+  );
+  res.status(201).send(location);
+});
+
+app.delete("/deletelocations/:id", async (req, res) => {
+  const id = req.params.id;
+  const location = await deleteLocation(id);
+  res.send(location);
+});
+//end of location
+
+//start of activities
+app.get("/activities/:id", async (req, res) => {
+  const id = req.params.id;
+  const activity = await getActivities(id);
+  res.send(activity);
+});
+
+app.post("/createactivities", async (req, res) => {
+  const { ActivityName, DurationTime, TotalCaloriesBurnt } = req.body;
+  const activity = await createActivities(
+    ActivityName,
+    DurationTime,
+    TotalCaloriesBurnt
+  );
+  res.status(201).send(activity);
+});
+
+app.put("/updateactivities", async (req, res) => {
+  const { ActivityName, DurationTime, TotalCaloriesBurnt, ActivityID } =
+    req.body;
+  const activity = await updateActivities(
+    ActivityName,
+    DurationTime,
+    TotalCaloriesBurnt,
+    ActivityID
+  );
+  res.status(201).send(activity);
+});
+
+app.delete("/deleteactivities/:id", async (req, res) => {
+  const id = req.params.id;
+  const activity = await deleteActivities(id);
+  res.send(activity);
+});
+//end of activities
+
+//start of trackactivities
+app.get("/tracksactivities/:id", async (req, res) => {
+  const id = req.params.id;
+  const trackactivity = await getTracksActivities(id);
+  res.send(trackactivity);
+});
+
+app.post("/createtracksactivities", async (req, res) => {
+  const { UserID, ActivityID } = req.body;
+  const trackactivity = await createTracksActivities(UserID, ActivityID);
+  res.status(201).send(trackactivity);
+});
+//end of trackactivities
+
+//start of activitylocation
+app.get("/activitylocation/:id", async (req, res) => {
+  const id = req.params.id;
+  const activitylocation = await getActivityLocation(id);
+  res.send(activitylocation);
+});
+
+app.post("/createactivitylocation", async (req, res) => {
+  const { UserID, LocationID } = req.body;
+  const activitylocation = await createActivityLocation(UserID, LocationID);
+  res.status(201).send(activitylocation);
+});
+//end of activitylocation
+
+//start of profile and profileID and userID are the same
+app.get("/profile/:id", async (req, res) => {
+  const id = req.params.id;
+  const profile = await getProfile(id);
+  res.send(profile);
+});
+
+app.post("/createprofile", async (req, res) => {
+  const { ProfileName, ProfileID } = req.body;
+  const profile = await createProfile(ProfileName, ProfileID);
+  res.status(201).send(profile);
+});
+
+app.put("/updateprofile", async (req, res) => {
+  const { ProfileName, ProfileID } = req.body;
+  const profile = await updateProfile(ProfileName, ProfileID);
+  res.status(201).send(profile);
+});
+//end of profile
+
+//start of milestones
+app.get("/milestones/:id", async (req, res) => {
+  const id = req.params.id;
+  const milestones = await getMilestones(id);
+  res.send(milestones);
+})
+
+app.post("/createmilestone", async (req, res) => {
+  const {Milestone, ProfileID} = req.body;
+  const milestone = await createMilestone(Milestone, ProfileID);
+  res.status(201).send(milestone);
+})
+
+app.put("/updatemilestones", async (req, res) => {
+  const {MilestoneID, Milestone} = req.body;
+  const milestone = await updateMilestone(MilestoneID, Milestone);
+  res.status(201).send(milestone);
+})
+
+app.delete("/deletemilestone", async (req, res) => {
+  const {MilestoneID} = req.body;
+  const milestone = await deleteMilestone(MilestoneID);
+  res.send(milestone);
+})
+//end of milestones
+
+// start of profilehasmilestones
+app.get("/profilemilestones/:id", async (req, res) => {
+  const id = req.params.id;
+  const profilemilestones = await getProfileMilestones(id);
+  res.send(profilemilestones);
+})
+
+app.post("/createprofilemilestones", async (req, res) => {
+  const {ProfileID, MilestoneID} = req.body;
+  const profilemilestones = await createProfileMilestones(ProfileID, MilestoneID);
+  res.status(201).send(profilemilestones);
+})
+
+//start of comment
+app.get("/comment/:id", async (req, res) => {
+  const id = req.params.id;
+  const comment = await getComment(id);
+  res.send(comment);
+})
+
+app.post("/createcomment", async (req, res) => {
+  const {ProfileID, Content, UserID, PublishDate} = req.body;
+  const comment = await createComment(ProfileID, Content, UserID, PublishDate);
+  res.status(201).send(comment);
+})
+
+app.put("/updatecomment", async (req, res) => {
+  const {ProfileID, Content, UserID, PublishDate} = req.body;
+  const comment = await updateComment(ProfileID, Content, UserID, PublishDate);
+  res.status(201).send(comment);
+})
+//end of comment
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+app.listen(8080, () => {
+  console.log("Server on port 8080");
+});
---
+++ backend/database.js
@@ -0,0 +1,427 @@
+import mysql from "mysql2";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const dbconnection = mysql
+  .createPool({
+    host: process.env.HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  })
+  .promise();
+
+//start of users
+export async function getUsers() {
+  const [result] = await dbconnection.query("SELECT * FROM User");
+  return result;
+}
+
+export async function getUser(id) {
+  const [result] = await dbconnection.query(
+    `
+    SELECT *
+    FROM User
+    WHERE UserID = ?`,
+    [id]
+  );
+  return result[0];
+}
+
+export async function getUserFromLogin(Username, Password) {
+  const [result] = await dbconnection.query(
+    `
+      SELECT UserID FROM User WHERE Username = ? AND Password = ?`,
+    [Username, Password]
+  );
+  return result[0];
+}
+
+export async function createUser(
+  Username,
+  Password,
+  FName,
+  LName,
+  DOB,
+  Weight,
+  Height,
+  Email
+) {
+  const [result] = await dbconnection.query(
+    `
+    INSERT INTO User (Username, Password, FName, LName, DOB, Weight, Height, Email)
+    VALUES(?,?,?,?,?,?,?,?)
+    `,
+    [Username, Password, FName, LName, DOB, Weight, Height, Email]
+  );
+  const id = result.insertId;
+  await createProfile(Username, id);
+  return getUser(id);
+}
+
+export async function updateUser(
+  Username,
+  Password,
+  FName,
+  LName,
+  DOB,
+  Weight,
+  Height,
+  Email,
+  UserID
+) {
+  const [result] = await dbconnection.query(
+    `
+        UPDATE User
+        SET Username = ?, Password = ?, FName = ?, LName = ?, DOB = ?, Weight = ?, Height = ?, Email = ?
+        WHERE UserID = ?
+    `,
+    [Username, Password, FName, LName, DOB, Weight, Height, Email, UserID]
+  );
+  return getUser(UserID);
+}
+
+export async function deleteUser(UserID) {
+  const [result] = await dbconnection.query(
+    `
+        DELETE FROM User WHERE UserID = ?
+    `,
+    [UserID]
+  );
+  return result.affectedRows;
+}
+//end of users
+
+//start of fitnessgoals
+export async function getFitnessGoalUser(id) {
+  const [result] = await dbconnection.query(
+    `SELECT * FROM FitnessGoal WHERE UserID = ?`,
+    [id]
+  );
+  return result[0];
+}
+
+export async function getFitnessGoal(id) {
+  const [result] = await dbconnection.query(
+    `SELECT * FROM FitnessGoal WHERE FitnessGoalID = ?`,
+    [id]
+  );
+  return result[0];
+}
+
+export async function createFitnessGoal(
+  UserID,
+  Descriptions,
+  CaloriesBurnt,
+  CaloriesConsumed
+) {
+  const [result] = await dbconnection.query(
+    `INSERT INTO FitnessGoal (UserID, Descriptions, CaloriesBurnt, CaloriesConsumed)
+    VALUES (?, ?, ?, ?)`,
+    [UserID, Descriptions, CaloriesBurnt, CaloriesConsumed]
+  );
+  const id = result.insertId;
+  return getFitnessGoal(id);
+}
+
+export async function updateFitnessGoal(
+  UserID,
+  Descriptions,
+  CaloriesBurnt,
+  CaloriesConsumed,
+  FitnessGoalID
+) {
+  const [result] = await dbconnection.query(
+    `
+    UPDATE FitnessGoal 
+    SET UserID = ?, Descriptions = ?, CaloriesBurnt = ?, CaloriesConsumed = ?
+    WHERE FitnessGoalID = ?`,
+    [UserID, Descriptions, CaloriesBurnt, CaloriesConsumed, FitnessGoalID]
+  );
+  return getFitnessGoal(FitnessGoalID);
+}
+
+export async function deleteFitnessGoal(FitnessGoalID) {
+  const [result] = await dbconnection.query(
+    `
+        DELETE FROM FitnessGoal WHERE FitnessGoalID = ?
+      `,
+    [FitnessGoalID]
+  );
+  return result.affectedRows;
+}
+//end of fitnessgoals
+
+//start of location
+export async function getLocation(id) {
+  const [result] = await dbconnection.query(
+    `SELECT * FROM Location WHERE LocationID = ?`,
+    [id]
+  );
+  return result[0];
+}
+
+export async function createLocation(Address, City, State, BuildingName) {
+  const [result] = await dbconnection.query(
+    `INSERT INTO Location (Address, City, State, BuildingName)
+    VALUES (?, ?, ?, ?)`,
+    [Address, City, State, BuildingName]
+  );
+  const id = result.insertId;
+  return getLocation(id);
+}
+
+export async function updateLocations(
+  Address,
+  City,
+  State,
+  BuildingName,
+  LocationID
+) {
+  const [result] = await dbconnection.query(
+    `
+    UPDATE Location 
+    SET Address = ?, City = ?, State = ?, BuildingName = ?
+    WHERE LocationID = ?`,
+    [Address, City, State, BuildingName, LocationID]
+  );
+  return getLocation(LocationID);
+}
+
+export async function deleteLocation(LocationID) {
+  const [result] = await dbconnection.query(
+    `
+    DELETE FROM Location WHERE LocationID = ?
+    `,
+    [LocationID]
+  );
+  return result.affectedRows;
+}
+//end of location
+
+//start of activities
+export async function getActivities(id) {
+  const [result] = await dbconnection.query(
+    `SELECT * FROM Activities WHERE ActivityID = ?`,
+    [id]
+  );
+  return result[0];
+}
+
+export async function createActivities(
+  ActivityName,
+  DurationTime,
+  TotalCaloriesBurnt
+) {
+  const [result] = await dbconnection.query(
+    `INSERT INTO Activities (ActivityName, DurationTime, TotalCaloriesBurnt)
+      VALUES (?, ?, ?)`,
+    [ActivityName, DurationTime, TotalCaloriesBurnt]
+  );
+  const id = result.insertId;
+  const userID = 1; // Temporary constant user ID
+  await createTracksActivities(userID, id); // Current user ID (possibly global) must be used for this function call
+  return getActivities(id);
+}
+
+export async function updateActivities(
+  ActivityName,
+  DurationTime,
+  TotalCaloriesBurnt,
+  ActivityID
+) {
+  const [result] = await dbconnection.query(
+    `
+    UPDATE Activities 
+    SET ActivityName = ?, DurationTime = ?, TotalCaloriesBurnt = ?
+    WHERE ActivityID = ?`,
+    [ActivityName, DurationTime, TotalCaloriesBurnt, ActivityID]
+  );
+  return getActivities(ActivityID);
+}
+
+export async function deleteActivities(ActivityID) {
+  const [result] = await dbconnection.query(
+    `
+    DELETE FROM Activities WHERE ActivityID = ?`,
+    [ActivityID]
+  );
+  return result.affectedRows;
+}
+//end of activities
+
+//start of trackactivities
+export async function getTracksActivities(id) {
+  const [result] = await dbconnection.query(
+    `SELECT * FROM TracksActivities WHERE UserID = ?`,
+    [id]
+  );
+  return result;
+}
+
+export async function createTracksActivities(UserID, ActivityID) {
+  const [result] = await dbconnection.query(
+    `INSERT INTO TracksActivities (UserID, ActivityID)
+    VALUES (?, ?)`,
+    [UserID, ActivityID]
+  );
+  return getTracksActivities(UserID);
+}
+//end of trackactivities
+
+//start of activitylocation
+export async function getActivityLocation(id) {
+  const [result] = await dbconnection.query(
+    `SELECT * FROM ActivityLocation WHERE UserID = ?`,
+    [id]
+  );
+  return result[0];
+}
+
+export async function createActivityLocation(UserID, LocationID) {
+  const [result] = await dbconnection.query(
+    `INSERT INTO ActivityLocation (UserID, LocationID)
+    VALUES (?, ?)`,
+    [UserID, LocationID]
+  );
+  return getActivityLocation(UserID);
+}
+//end of activitylocation
+
+//start of profile
+export async function getProfile(id) {
+  const [result] = await dbconnection.query(
+    `SELECT * FROM Profile WHERE ProfileID = ?`,
+    [id]
+  );
+  return result[0];
+}
+
+export async function createProfile(ProfileName, UserID) {
+  const [result] = await dbconnection.query(
+    `INSERT INTO Profile (ProfileID, ProfileName)
+    VALUES (?, ?)`,
+    [UserID, ProfileName]
+  );
+  return getProfile(UserID);
+}
+
+export async function updateProfile(ProfileName, UserID) {
+  const [result] = await dbconnection.query(
+    `
+    UPDATE Profile 
+    SET ProfileName = ?
+    WHERE ProfileID = ?`,
+    [ProfileName, UserID]
+  );
+  return getProfile(UserID);
+}
+//end of profile
+
+
+
+//start of milestones
+export async function getMilestones(id) {
+    const [result] = await dbconnection.query(
+        `
+        SELECT * FROM Milestones WHERE MilestoneID = ?`,
+        [id]
+    );
+    return result[0];
+}
+
+export async function createMilestone(Milestone, ProfileID) {
+    const [result] = await dbconnection.query(
+        `INSERT INTO Milestones (Milestone)
+        VALUES (?)`,
+        [Milestone]
+    );
+    const id = result.insertId;
+    await createProfileMilestones(ProfileID, id);
+    return getMilestones(id);
+}
+
+export async function updateMilestone(MilestoneID, Milestone) {
+    const [result] = await dbconnection.query(
+        `
+        UPDATE Milestones
+        SET Milestone = ?
+        WHERE MilestoneID = ?`,
+        [Milestone, MilestoneID]
+    );
+    return getMilestones(MilestoneID);
+}
+
+export async function deleteMilestone(MilestoneID) {
+    const [result] = await dbconnection.query(
+        `
+        DELETE FROM Milestones WHERE MilestoneID = ?`,
+        [MilestoneID]
+    );
+    return result.affectedRows;
+}
+//end of milestones
+
+// I'm not sure if we need this, so I'll leave the functions for ProfileHasMilestones for now.
+// start of profilehasmilestones
+export async function getProfileMilestones(id) {
+    const [result] = await dbconnection.query(
+        `
+        SELECT * FROM ProfileHasMilestones WHERE ProfileID = ?`,
+        [id]
+    );
+    return result[0];
+}
+
+export async function createProfileMilestones(ProfileID, MilestoneID) {
+    const [result] = await dbconnection.query(
+        `
+        INSERT INTO ProfileHasMilestones (ProfileID, MilestoneID)
+        VALUES (?, ?)`,
+        [ProfileID, MilestoneID]
+    );
+    return getProfileMilestones(ProfileID);
+}
+//end of profilehasmilestones
+
+//start of comment
+export async function getComment(id) {
+    const [result] = await dbconnection.query(
+        `
+        SELECT * FROM Comment WHERE ProfileID = ?`,
+        [id]
+    );
+    return result; // Show all comments a user has made?
+}
+
+export async function createComment(
+    ProfileID, 
+    Content,
+    UserID,
+    PublishDate
+) {
+    const [result] = await dbconnection.query(
+        `
+        INSERT INTO Comment(ProfileID, Content, UserID, PublishDate)
+        VALUES (?, ?, ?, ?)`,
+        [ProfileID, Content, UserID, PublishDate]
+    );
+    return getComment(ProfileID);
+}
+
+export async function updateComment(
+    ProfileID,
+    Content,
+    UserID,
+    PublishDate
+) {
+    const [result] = await dbconnection.query(
+        `
+        UPDATE Comment
+        SET ProfileID = ?, Content = ?, UserID = ?, PublishDate = ?`,
+        [ProfileID, Content, UserID, PublishDate]
+    );
+    return getComment(ProfileID);
+}
+//end of comment
