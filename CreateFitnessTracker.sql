4777577b9e1194dd40dd088db1ebf7bbb0f00b08
AxisBreak <namdinhbui@gmail.com>
2024-05-07 20:26:07 -0700

    moved the example data to another file

--- CreateFitnessTracker.sql
+++ CreateFitnessTracker.sql
@@ -87,27 +87,3 @@ CREATE INDEX LocationIndex USING BTREE ON Location(LocationID);
 CREATE INDEX ActivitiesIndex USING BTREE ON Activities(ActivityID);
 CREATE INDEX ProfileIndex USING BTREE ON Profile(ProfileID);
 CREATE INDEX MilestonesIndex USING BTREE ON Milestones(MilestoneID);
-
--- example
-INSERT INTO User (Username, Password, FName, LName, DOB, Weight, Height, Email) 
-VALUES ('bob', 'basd', 'dsf', 'dsf', '1985-05-15', 'dsf', 'dsf', 'dsf');
-
-INSERT INTO FitnessGoal (UserID, Descriptions, CaloriesBurnt, CaloriesConsumed)
-VALUES (1, "blob", 100,  100);
-
-INSERT INTO Location (Address, City, State, BuildingName)
-VALUES ("bobadb", "dsfadsf", "asda", "A");
-
-INSERT INTO Location (Address, City, State, BuildingName)
-VALUES ("bobadb", "dsfadsf", "asda", "A");
-
-INSERT INTO Activities (ActivityName, DurationTime, TotalCaloriesBurnt)
-VALUES ("asdf", 23, 32);
-
-INSERT INTO Profile (ProfileID, ProfileName)
-VALUES (1, "bibly")
-
-
-
-
-
